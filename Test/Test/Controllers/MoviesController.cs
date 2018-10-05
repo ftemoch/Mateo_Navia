cb688ca6a8f2f0c9e8e6fa226074e7fa221545b2
Mateo <Mateo@LAPTOP-S86UDVR2>
2018-10-05 11:35:17 -0500

    Profe, aqui esta lo que hicimos hoy en clase

---
+++ Test/Test/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.Controllers;
+
+namespace Test.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var Movie = new Movie()
+            {
+                Nombre = "Friend request!"
+            };
+            return View(Movie);
+        }
+    }
+}
---
+++ Test/Test/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Movie
+    {
+        public int MyProperty { get; set; }
+        public String Nimbre { get; set; }
+    }
+}
