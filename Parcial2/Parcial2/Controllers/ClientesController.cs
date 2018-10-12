38d0829f4357b0e140f191fa5ef7d8ab0b2b3b7d
Mateo <Mateo@LAPTOP-S86UDVR2>
2018-10-12 10:00:38 -0500

    Parcial 2 complete

---
+++ Parcial2/Parcial2/Controllers/ClientesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Random()
+        {
+            var Cliente = new Clientes()
+            {
+                Nombre = "John",
+                Apellido = "Smith",
+                Sueldo = 200
+            };
+            return View(Cliente);
+        }
+    }
+}
---
+++ Parcial2/Parcial2/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public Double Sueldo { get; set; }
+    }
+}
