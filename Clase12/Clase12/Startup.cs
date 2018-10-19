a70c033bba017c829f155db95358fd3148f2d208
Fernando Temoch <ftemoche@hotmail.com>
2018-10-18 23:57:57 -0500

    Clase 12  19/Octubre

---
+++ Clase12/Clase12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clase12.Startup))]
+namespace Clase12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
